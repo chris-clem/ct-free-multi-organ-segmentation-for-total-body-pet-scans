2a08af42b8425f7db112673b23e4c530fd355ba1
Christoph Clement <christoph.clement@students.unibe.ch>
2023-12-05 17:50:53 +0100

    Store in ts dir

--- pet_seg/01_run_totalsegmentator.py
+++ pet_seg/01_run_totalsegmentator.py
@@ -3,6 +3,7 @@ from loguru import logger
 from totalsegmentator.python_api import totalsegmentator
 from tqdm import tqdm
 
+from pet_seg.settings import DATA_ROOT_DIR
 from pet_seg.utils import get_sorted_patient_dirs
 
 
@@ -14,18 +15,24 @@ def run_totalsegmentator(
     scanner: str,
 ):
     """Creates CT-based segmentation masks with TotalSegmentator for each patient for a given scanner."""
+
+    output_dir = DATA_ROOT_DIR / "totalsegmentator" / scanner
+    output_dir.mkdir(parents=True, exist_ok=True)
+
     for patient_dir in tqdm(get_sorted_patient_dirs(scanner)):
         patient_id = patient_dir.name
 
-        if (ts_seg_path := patient_dir / "CT_TS_seg.nii.gz").exists():
+        if (ts_seg_path := output_dir / f"{patient_id}.nii.gz").exists():
             logger.debug(f"Skipping {patient_id} because {ts_seg_path.name} already exists")
             continue
 
-        logger.debug(f"Running TotalSegmentator for {patient_id}")
+        if not (ct_path := patient_dir / "CT.nii.gz").exists():
+            logger.warning(f"Skipping {patient_id} because {ct_path.name} does not exist")
+            continue
 
         try:
             totalsegmentator(
-                input=patient_dir / "CT.nii.gz",
+                input=ct_path,
                 output=ts_seg_path,
                 ml=True,
                 nr_thr_resamp=4,
