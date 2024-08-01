aa4b1ba70a658d89951d9a41e8f63c69501460be
Christoph Clement <christoph.clement@students.unibe.ch>
2024-08-01 15:19:01 +0200

    Parallelize and create separate dir

--- pet_seg/optimized_label_scripts/change_optimized_label_order.py
+++ pet_seg/optimized_label_scripts/change_optimized_label_order.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import fire
 import nibabel as nib
 import numpy as np
+from joblib import delayed
+from joblib import Parallel
 from tqdm import tqdm
 
 OPTIMIZED_LABELS_TO_ORGANS = {
@@ -24,7 +26,7 @@ OPTIMIZED_LABELS_TO_ORGANS = {
 OPTIMIZED_TO_TS_LABEL_MAPPING = {
     0: 0,
     # 1: 8,
-    # 2: 52,
+    2: 52,
     3: 21,
     4: 90,
     5: 51,
@@ -37,26 +39,59 @@ OPTIMIZED_TO_TS_LABEL_MAPPING = {
     12: 10,
 }
 
+OPTIMIZED_TO_TS_MERGED_LABEL_MAPPING = {
+    0: 0,
+    # 1: 7,
+    2: 21,
+    3: 15,
+    4: 41,
+    5: 20,
+    6: 2,
+    7: 4,
+    8: 6,
+    9: 1,
+    10: 11,
+    # 11: 28,
+    12: 8,
+}
+
+TS_TO_OPTIMIZED_LABEL_MAPPING = {v: k for k, v in OPTIMIZED_TO_TS_MERGED_LABEL_MAPPING.items()}
+
 
 def main():
     fire.Fire(change_optimized_label_order)
 
 
 def change_optimized_label_order(
-    optimized_label_dir: str = "/home/user/Data/ct-free-multi-organ-segmentation-for-total-body-pet-scans/nnunetv2/raw/Dataset100_Bern_Quadra-SH_uExplorer-num_train=956-num_test=50_NAC/labelsTs_optimized",  # noqa: E501
+    optimized_label_dir: str,
+    n_jobs: int = 1,
 ):
     optimized_label_dir = Path(optimized_label_dir)
 
-    for optimized_label_path in tqdm(sorted(optimized_label_dir.glob("*.nii.gz"))):
-        optimized_label_nii = nib.load(optimized_label_path)
-        optimized_label_npy = optimized_label_nii.get_fdata()
+    optimized_label_dir_changed_order = optimized_label_dir.parent / f"{optimized_label_dir.name}_changed_order"
+    optimized_label_dir_changed_order.mkdir(parents=True, exist_ok=True)
 
-        optimized_label_ts_npy = np.zeros_like(optimized_label_npy)
-        for optimized_label, ts_label in OPTIMIZED_TO_TS_LABEL_MAPPING.items():
-            optimized_label_ts_npy[optimized_label_npy == optimized_label] = ts_label
+    Parallel(n_jobs=n_jobs)(
+        delayed(process_one)(optimized_label_path, optimized_label_dir_changed_order)
+        for optimized_label_path in tqdm(sorted(optimized_label_dir.glob("*.nii.gz")))
+    )
 
-        optimized_label_ts_nii = nib.Nifti1Image(optimized_label_ts_npy, optimized_label_nii.affine)
-        nib.save(optimized_label_ts_nii, optimized_label_path)
+
+def process_one(optimized_label_path, optimized_label_dir_changed_order):
+    optimized_label_nii = nib.load(optimized_label_path)
+    optimized_label_npy = optimized_label_nii.get_fdata()
+
+    # print(f"unique labels: {np.unique(optimized_label_npy)}")
+
+    optimized_label_ts_npy = np.zeros_like(optimized_label_npy)
+    for optimized_label, ts_label in OPTIMIZED_TO_TS_MERGED_LABEL_MAPPING.items():
+        optimized_label_ts_npy[optimized_label_npy == optimized_label] = ts_label
+
+    # print(f"unique labels: {np.unique(optimized_label_ts_npy)}")
+
+    optimized_label_ts_path = optimized_label_dir_changed_order / optimized_label_path.name
+    optimized_label_ts_nii = nib.Nifti1Image(optimized_label_ts_npy, optimized_label_nii.affine)
+    nib.save(optimized_label_ts_nii, optimized_label_ts_path)
 
 
 if __name__ == "__main__":
