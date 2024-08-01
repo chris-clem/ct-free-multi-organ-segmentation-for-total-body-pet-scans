dd0e32e25955d2ee3d32df841fbd593b3920f2d2
Christoph Clement <christoph.clement@students.unibe.ch>
2024-08-01 15:19:39 +0200

    Extract Dice and IoU

--- pet_seg/optimized_label_scripts/extract_ts_vs_optimized_results.py
+++ pet_seg/optimized_label_scripts/extract_ts_vs_optimized_results.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import fire
 
 from pet_seg.settings import RESULTS_DIR
-from pet_seg.utils import create_patient_dice_scores_df
+from pet_seg.utils import create_patient_metrics_df
 
 
 def main():
@@ -11,10 +11,18 @@ def main():
 
 
 def extract_ts_vs_optimized_results(
-    ts_vs_optimized_summary_path: str = "/home/user/Data/ct-free-multi-organ-segmentation-for-total-body-pet-scans/nnunetv2/raw/Dataset001_Bern_Quadra-SH_uExplorer-num_train=956-num_test=50_NAC/labelsTs_optimized/summary_labelsTs.json",  # noqa: E501
+    ts_vs_optimized_summary_path: str,
 ):
-    patient_dice_scores_df = create_patient_dice_scores_df(Path(ts_vs_optimized_summary_path))
-    patient_dice_scores_df.to_csv(RESULTS_DIR / "patient_dice_scores" / "ts_vs_optimized_labels.csv", index=False)
+    for metric in ["Dice", "IoU"]:
+        patient_dice_scores_df = create_patient_metrics_df(
+            Path(ts_vs_optimized_summary_path),
+            use_merged_seg="merged" in ts_vs_optimized_summary_path,
+            use_optimized_seg=True,
+            metric=metric,
+        )
+        patient_dice_scores_df.to_csv(
+            RESULTS_DIR / "patient_metrics" / f"ts_vs_optimized_labels__{metric}.csv", index=False
+        )
 
 
 if __name__ == "__main__":
